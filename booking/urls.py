d7d117376256fb8d8ee5a9803e5f99f3c1529ce5
KostyaNike <deras1234oo@gmail.com>
2024-09-24 17:31:03 +0200

    done

--- booking/urls.py
+++ booking/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from booking import views
 
 urlpatterns = [
-    path('rooms/', views.room_list, name='room_list'),
+    path('rooms/', views.room_list, name='room-list'),
+    path('book-room/', views.book_room, name='book-room'),
+    path('booking-details/<int:pk>', views.booking_details, name='booking-details')
 ]
+
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Room
+from django.shortcuts import render, redirect
+from .models import Room, Booking
+from django.http import HttpResponse
 
 def room_list(request):
     rooms = Room.objects.all()
@@ -7,3 +8,44 @@ def room_list(request):
         'rooms': rooms,
     }
     return render(request, "booking/room_list.html", context)
+
+def book_room(request):
+    if request.method == "POST":
+        room_number = request.POST.get("room-number")
+        start_time = request.POST.get("start-time")
+        end_time = request.POST.get("end-time")
+
+        try:
+            room = Room.objects.get(number=room_number)
+        except ValueError:
+            return HttpResponse(
+                "Wrong value for room number!",
+                status=400
+            )
+        except Room.DoesNotExist:
+            return HttpResponse(
+                "This room number doesn't exist",
+                status=404
+            )
+        booking = Booking.objects.create(
+            user=request.user,
+            room=room,
+            start_time=start_time,
+            end_time=end_time
+        )
+        return redirect("booking-details", pk=booking.id)
+    else: 
+        return render(request, template_name="booking/booking_form.html")
+
+def booking_details(request, pk):
+    try:
+        booking = Booking.objects.get(id=pk)
+        context = {
+            "booking": booking
+        }
+        return render(request, template_name="booking/booking_details.html", context=context)
+    except Booking.DoesNotExist:
+        return HttpResponse(
+            "This booking doesn't exist",
+            status=404
+        )
